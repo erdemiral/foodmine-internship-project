920425a96b6b78164fc7e98e44145c3324895ae8
erdemiral <hikmeterdemiral@gmail.com>
2023-08-01 13:55:40 +0300

    login page done

--- frontend/src/app/components/pages/login-page/login-page.component.ts
+++ frontend/src/app/components/pages/login-page/login-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-login-page',
@@ -9,8 +11,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginPageComponent implements OnInit {
   loginForm!:FormGroup
   isSubmitted=false
-
-  constructor(private formBuilder:FormBuilder){}
+  returnUrl=''
+  constructor(private formBuilder:FormBuilder, 
+    private userService:UserService,private activatedRoute:ActivatedRoute,private router:Router){}
 
 
   ngOnInit(): void {
@@ -18,6 +21,8 @@ export class LoginPageComponent implements OnInit {
         email:['', [Validators.required,Validators.email]],
         password:['', Validators.required]
       })
+
+      this.returnUrl=this.activatedRoute.snapshot.queryParams.returnUrl
   }
 
   get fc(){
@@ -31,6 +36,10 @@ export class LoginPageComponent implements OnInit {
     alert(`email: ${this.fc.email.value},
      password: ${this.fc.password.value}`) 
 
+     this.userService.login({email:this.fc.email.value,
+    password:this.fc.password.value}).subscribe(()=> {
+      this.router.navigateByUrl(this.returnUrl)
+    })
   }
 
 }
