e8c37f758db08ee4af04fb38559b3e1016ad265e
erdemiral <hikmeterdemiral@gmail.com>
2023-08-02 16:08:39 +0300

    error

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,13 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
-import { sample_foods, sample_tags, sample_users } from "./data";
-import jwt from "jsonwebtoken";
 import foodRouter from './routers/food.router'
 import userRouter from './routers/user.router'
+import { dbConnect } from './configs/database.config';
+dbConnect()
+
 const app = express();
 app.use(express.json())
 app.use(cors({
