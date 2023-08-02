38304fcf361d5a599820f0f384bdabb7f26f7a73
erdemiral <hikmeterdemiral@gmail.com>
2023-08-02 17:35:10 +0300

    fixed all

--- backend/src/routers/food.router.ts
+++ backend/src/routers/food.router.ts
@@ -1,81 +1,82 @@
-import { Router } from "express";
-import { sample_foods, sample_tags } from "../data";
-import asynceHandler from "express-async-handler";
-import { FoodModel } from "../models/food.model";
+import {Router} from 'express';
+import { sample_foods, sample_tags } from '../data';
+import asyncHandler from 'express-async-handler';
+import { FoodModel } from '../models/food.model';
 const router = Router();
 
-router.get("/seed",asynceHandler(
-  async (req, res) => {
-    const foodsCount= await FoodModel.countDocuments()
-    if(foodsCount>0){
-      res.send("Seed is already done!")
-      return
+router.get("/seed", asyncHandler(
+ async (req, res) => {
+    const foodsCount = await FoodModel.countDocuments();
+    if(foodsCount> 0){
+      res.send("Seed is already done!");
+      return;
     }
 
-    await FoodModel.create(sample_foods)
-    res.send("Seed Is Done!")
-    
-  })
-);
+    await FoodModel.create(sample_foods);
+    res.send("Seed Is Done!");
+}
+))
 
-router.get("/", asynceHandler(
+
+router.get("/",asyncHandler(
   async (req, res) => {
-    const foods=await FoodModel.find()
-    res.send(sample_foods);
+    const foods = await FoodModel.find();
+      res.send(foods);
   }
-));
-router.get("/search/:searchTerm", asynceHandler(
+))
+
+router.get("/search/:searchTerm", asyncHandler(
   async (req, res) => {
-    const searchRegex=new RegExp(req.params.searchTerm, 'i')
-    const foods= await FoodModel.find({name:{$regex:searchRegex}})
+    const searchRegex = new RegExp(req.params.searchTerm, 'i');
+    const foods = await FoodModel.find({name: {$regex:searchRegex}})
     res.send(foods);
   }
-));
+))
 
-router.get("/tags", asynceHandler(
+router.get("/tags", asyncHandler(
   async (req, res) => {
-    const tags= await FoodModel.aggregate([{
-      $unwind:'$tags'
-      //2 foods 3 tags, unwind tags => 6 foods tags 1
-    },
-    {
-      $group:{
-        _id:'$tags',
-        count:{$sum:1}
+    const tags = await FoodModel.aggregate([
+      {
+        $unwind:'$tags'
+      },
+      {
+        $group:{
+          _id: '$tags',
+          count: {$sum: 1}
+        }
+      },
+      {
+        $project:{
+          _id: 0,
+          name:'$_id',
+          count: '$count'
+        }
       }
-    },
-    {
-      $project:{
-        _id:0,
-        name:'$_id',
-        count:'$count'
-      }
-    }
+    ]).sort({count: -1});
 
-    ]).sort({count: -1})
-
-    const all={
-      name: 'All',
+    const all = {
+      name : 'All',
       count: await FoodModel.countDocuments()
     }
 
-    tags.unshift(all)  //video16
+    tags.unshift(all);
     res.send(tags);
   }
-));
+))
 
-router.get("/tag/:tagName", asynceHandler(
-  async(req, res) => {
-    const foods= await FoodModel.find({tags: req.params.tagName})
+router.get("/tag/:tagName",asyncHandler(
+  async (req, res) => {
+    const foods = await FoodModel.find({tags: req.params.tagName})
     res.send(foods);
   }
-));
+))
 
-router.get("/:foodId", asynceHandler(
+router.get("/:foodId", asyncHandler(
   async (req, res) => {
-    const food=await FoodModel.findById(req.params.foodId)
+    const food = await FoodModel.findById(req.params.foodId);
     res.send(food);
   }
-));
+))
+
 
 export default router;
