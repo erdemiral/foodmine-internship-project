4279a945819d97a8b321753bdf949807d6c1558c
erdemiral <hikmeterdemiral@gmail.com>
2023-08-03 13:01:35 +0300

    add register

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -2,9 +2,10 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { BehaviorSubject, Observable, tap } from 'rxjs';
-import { USER_LOGIN_URL } from '../shared/models/constants/urls';
+import { USER_LOGIN_URL, USER_REGISTER_URL } from '../shared/models/constants/urls';
 import { IUserLogin } from '../shared/interfaces/IUserLogin';
 import { User } from '../shared/models/User';
+import { IUserRegister } from '../shared/interfaces/IUserRegister';
 
 const USER_KEY = 'User';
 @Injectable({
@@ -34,6 +35,25 @@ export class UserService {
         }
       })
     );
+  }
+
+
+  register(userRegister:IUserRegister):Observable<User>{
+    return this.http.post<User>(USER_REGISTER_URL,userRegister).pipe(
+      tap({
+        next:(user)=>{
+          this.setUserToLocalStorage(user)
+          this.userSubject.next(user)
+          this.toastrService.success(
+            `Welcome to the Foodmine ${user.name}`,
+           'Register Successful' )
+        },
+        error:(errorResponse)=>{
+          this.toastrService.error(errorResponse.error,
+            'Register Failed')
+        }
+      })
+    )
   }
 
   logout(){
